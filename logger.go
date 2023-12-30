90cad5304b20a6158b8b4f2b58b5d510312da97f
jsn4ke <mail.caoke@gmail.com>
2023-12-30 14:17:12 +0800

    fix time format

--- logger.go
+++ logger.go
@@ -30,17 +30,17 @@ type defaultFmtLogger struct{}
 
 // Fatal implements Logger.
 func (*defaultFmtLogger) Fatal(format string, params ...any) {
-	fmt.Printf("[%v][Fatal]"+format+"\n", append(append([]any{}, time.Now().Format("2023-12-30 13:41:21.730267")), params...)...)
+	fmt.Printf("[%v][Fatal]"+format+"\n", append(append([]any{}, time.Now().Format("2006-01-02 15:04:05.999999")), params...)...)
 	os.Exit(2)
 }
 
 // Warn implements Logger.
 func (*defaultFmtLogger) Warn(format string, params ...any) {
-	fmt.Printf("[%v][Warn]"+format+"\n", append(append([]any{}, time.Now().Format("2023-12-30 13:41:21.730267")), params...)...)
+	fmt.Printf("[%v][Warn]"+format+"\n", append(append([]any{}, time.Now().Format("2006-01-02 15:04:05.999999")), params...)...)
 }
 
 func (d defaultFmtLogger) Debug(format string, params ...any) {
-	fmt.Printf("[%v][Debug]"+format+"\n", append(append([]any{}, time.Now().Format("2023-12-30 13:41:21.730267")), params...)...)
+	fmt.Printf("[%v][Debug]"+format+"\n", append(append([]any{}, time.Now().Format("2006-01-02 15:04:05.999999")), params...)...)
 }
 
 func (d defaultFmtLogger) Panic(format string, params ...any) {
@@ -48,9 +48,9 @@ func (d defaultFmtLogger) Panic(format string, params ...any) {
 }
 
 func (d defaultFmtLogger) Info(format string, params ...any) {
-	fmt.Printf("[%v][Info]"+format+"\n", append(append([]any{}, time.Now().Format("2023-12-30 13:41:21.730267")), params...)...)
+	fmt.Printf("[%v][Info]"+format+"\n", append(append([]any{}, time.Now().Format("2006-01-02 15:04:05.999999")), params...)...)
 }
 
 func (d defaultFmtLogger) Error(format string, params ...any) {
-	fmt.Printf("[%v][Error]"+format+"\n", append(append([]any{}, time.Now().Format("2023-12-30 13:41:21.730267")), params...)...)
+	fmt.Printf("[%v][Error]"+format+"\n", append(append([]any{}, time.Now().Format("2006-01-02 15:04:05.999999")), params...)...)
 }
